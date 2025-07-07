0fc6c4f7743a80cb3841b875c647cfc032cc690f
Náyra Zanini <106480562+nayrazanini@users.noreply.github.com>
2025-07-07 12:52:57 -0300

    estudo

--- estudos/estudos-funcoes.js
+++ estudos/estudos-funcoes.js
@@ -1,5 +1,5 @@
 
-//Crie uma função verificarIdade que receba uma idade e diga se a pessoa é maior de idade ou não.
+//a) Crie uma função verificarIdade que receba uma idade e diga se a pessoa é maior de idade ou não.
 
 
 function verificarIdade (idade){
@@ -12,7 +12,7 @@ function verificarIdade (idade){
 
 verificarIdade(20);
 
-/* Crie uma função chamada verificarNota que receba um número de 0 a 10 representando a nota de um aluno.
+/* b) Crie uma função chamada verificarNota que receba um número de 0 a 10 representando a nota de um aluno.
 A função deve seguir esta lógica:
 Se a nota for maior ou igual a 7, exibir: "Aprovado"
 Se a nota for maior ou igual a 5, mas menor que 7, exibir: "Recuperação"
@@ -37,3 +37,43 @@ verificarNota(10);  // Aprovado
 verificarNota(6);   // Recuperação
 verificarNota(3);   // Reprovado
 verificarNota(11);  // Nota Inválida
+
+
+/* c) Crie uma função chamada calcularIMC que receba dois parâmetros:
+
+peso (em kg)
+
+altura (em metros)
+
+A função deve calcular o IMC com a fórmula: IMC = peso / (altura * altura)
+
+E depois exibir no console a classificação de acordo com a tabela abaixo:
+
+IMC	Classificação
+Menor que 18.5	Abaixo do peso
+Entre 18.5 e 24.9	Peso normal
+Entre 25 e 29.9	Sobrepeso
+30 ou mais	Obesidade
+
+*/
+
+function calcularIMC (peso, altura){
+    let IMC = (peso/(altura*altura))
+    if (IMC < 18.5){
+        classificacao = ("Abaixo do peso")
+    } else if (IMC >=18.5 && IMC <=24.9){
+        classificacao = ("Peso Normal")
+    } else if (IMC >=25 && IMC <=29.9){
+        classificacao = ("Sobrepeso")
+    } else{
+        classificacao= ("Obesidade")
+    }
+
+}
+
+calcularIMC (60, 1.65)
+    console.log("Seu IMC É " + classificacao)
+calcularIMC (30, 1.80)
+    console.log("Seu IMC É " + classificacao)
+calcularIMC (90, 1.50)
+    console.log("Seu IMC É " + classificacao)
